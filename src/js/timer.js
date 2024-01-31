759dcea461260bf7db6e9d83a717b7df60abfc1e
Mona LisaVladyslav <143653897+VladyslavPunko@users.noreply.github.com>
2024-01-31 23:21:27 +0100

    агдд

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,2 +1,80 @@
-const x = 0;
-console.log(x);
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import closeIcon from '../img/bi_x-octagon.png';
+
+const inputPicker = document.querySelector('#datetime-picker');
+const buttonStart = document.querySelector('[data-start]');
+const daysTimer = document.querySelector('[data-days]');
+const hoursTimer = document.querySelector('[data-hours]');
+const minutesTimer = document.querySelector('[data-minutes]');
+const secondsTimer = document.querySelector('[data-seconds]');
+
+buttonStart.disabled = true;
+let userSelectedDate = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0].getTime();
+    buttonStart.disabled = false;
+
+    if (userSelectedDate < Date.now()) {
+      buttonStart.disabled = true;
+      iziToast.error({
+        messageColor: '#FFF',
+        color: '#EF4040',
+        iconUrl: closeIcon,
+        position: 'topRight',
+        message: 'Please choose a date in the future',
+      });
+    }
+  },
+};
+
+flatpickr(inputPicker, options);
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+
+buttonStart.addEventListener('click', onClickStart);
+
+function onClickStart() {
+  buttonStart.disabled = true;
+  const interval = setInterval(() => {
+    let timeToLeft = userSelectedDate - Date.now();
+    inputPicker.disabled = true;
+
+    if (timeToLeft <= 0) {
+      clearInterval(intervalId);
+      inputPicker.disabled = false;
+      iziToast.info({ position: 'center', message: 'It is your time!' });
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(timeToLeft);
+    daysTimer.textContent = `${addLeadingZero(days)}`;
+    hoursTimer.textContent = `${addLeadingZero(hours)}`;
+    minutesTimer.textContent = `${addLeadingZero(minutes)}`;
+    secondsTimer.textContent = `${addLeadingZero(seconds)}`;
+  }, 1000);
+}
